b16178a80961f18e15f698fd4f8738f078b92585
Vinicius Bueno <vi-pr@hotmail.com>
2023-10-18 15:07:29 -0300

    Add javascript

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const openFormButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close-button");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const inputName = document.querySelector(".popup__input_type_name");
+const inputAbout = document.querySelector(".popup__input_type_about");
+const form = document.querySelector(".popup__form");
+
+openFormButton.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+}
+form.addEventListener("submit", handleFormSubmit);
+
+// function toggleForm() {
+//   inputName.value = profileName.textContent;
+//   inputAbout.value = profileAbout.textContent;
+//   popup.classList.add(".popup__visible");
+// }
