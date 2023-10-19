d658004ed055564b41b5c5a0ecbc3fd890ef27b0
Vinicius Bueno <vi-pr@hotmail.com>
2023-10-19 20:02:52 -0300

    fix java and css

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,8 @@ const inputAbout = document.querySelector(".popup__input_type_about");
 const form = document.querySelector(".popup__form");
 
 openFormButton.addEventListener("click", () => {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
   popup.style.display = "block";
 });
 
@@ -19,11 +21,6 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
+  popup.style.display = "none";
 }
 form.addEventListener("submit", handleFormSubmit);
-
-// function toggleForm() {
-//   inputName.value = profileName.textContent;
-//   inputAbout.value = profileAbout.textContent;
-//   popup.classList.add(".popup__visible");
-// }
