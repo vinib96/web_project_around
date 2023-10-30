e53acbe552bba3b36c0e4c8b0407efaf2591674d
Vinicius Bueno <vi-pr@hotmail.com>
2023-10-30 17:02:53 -0300

    initial config

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
 const openFormButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
+const openAddButton = document.querySelector(".profile__add-button");
+const popup = document.querySelector(".popup__profile");
+const addCards = document.querySelector(".popup__add-cards");
 const closeButton = popup.querySelector(".popup__close-button");
+const closeAdd = addCards.querySelector(".popup__close-add");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__about");
 const inputName = document.querySelector(".popup__input_type_name");
 const inputAbout = document.querySelector(".popup__input_type_about");
 const form = document.querySelector(".popup__form");
 
-openFormButton.addEventListener("click", function () {
+openFormButton.addEventListener("click", () => {
   popup.classList.add("popup__opened");
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 });
 
-closeButton.addEventListener("click", function () {
+openAddButton.addEventListener("click", () => {
+  addCards.classList.add("popup__opened");
+});
+
+closeButton.addEventListener("click", () => {
   popup.classList.remove("popup__opened");
+});
+
+closeAdd.addEventListener("click", () => {
+  addCards.classList.remove("popup__opened");
 });
 
 function handleFormSubmit(evt) {
