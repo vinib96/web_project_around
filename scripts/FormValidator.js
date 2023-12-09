340320faa05650a472229b666d4ccab97ecc4c91
Vinicius Bueno <vi-pr@hotmail.com>
2023-12-09 08:17:53 -0300

    error formvalidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -45,8 +45,7 @@ export default class FormValidator {
       buttonElement.classList.remove(this._config.inactiveButtonClass);
     }
   }
-
-  _fieldsetEvent() {
+  _setEventListeners() {
     const buttonElement = this._formElement.querySelector(
       this._config.submitButtonSelector
     );
@@ -61,8 +60,40 @@ export default class FormValidator {
       });
     });
   }
+  // _fieldsetEvent() {
+  //   const buttonElement = this._formElement.querySelector(
+  //     this._config.submitButtonSelector
+  //   );
+  //   const inputList = Array.from(
+  //     this._formElement.querySelectorAll(this._config.inputSelector)
+  //   );
+  //   this._toggleButtonState(inputList, buttonElement);
+  //   inputList.forEach((inputElement) => {
+  //     inputElement.addEventListener("input", () => {
+  //       this._checkInputValidity(this._formElement, inputElement);
+  //       this._toggleButtonState(inputList, buttonElement);
+  //     });
+  //   });
+  // }
 
-  _setEventListeners(formList) {
+  // _setEventListeners(formList) {
+  //   formList.forEach((formElement) => {
+  //     formElement.addEventListener("submit", (evt) => {
+  //       evt.preventDefault();
+  //     });
+  //     const fieldsetList = Array.from(
+  //       this._formElement.querySelectorAll(".popup__formset")
+  //     );
+  //     fieldsetList.forEach((fieldset) => {
+  //       this._fieldsetEvent(fieldset);
+  //     });
+  //   });
+  // }
+
+  enableValidation() {
+    const formList = Array.from(
+      document.querySelectorAll(this._config.formSelector)
+    );
     formList.forEach((formElement) => {
       formElement.addEventListener("submit", function (evt) {
         evt.preventDefault();
@@ -73,16 +104,13 @@ export default class FormValidator {
       );
 
       fieldsetList.forEach((fieldset) => {
-        this._fieldsetEvent(fieldset);
+        this._setEventListeners(fieldset);
       });
     });
-  }
-
-  enableValidation() {
-    const formList = Array.from(
-      document.querySelectorAll(this._config.formSelector)
-    );
-    this._setEventListeners(formList);
+    // const formList = Array.from(
+    //   document.querySelectorAll(this._config.formSelector)
+    // );
+    // this._setEventListeners(formList);
   }
 }
 const addCards = document.querySelector(".popup_add-cards");
@@ -96,7 +124,6 @@ const profileValid = new FormValidator(
     inactiveButtonClass: "popup__button_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible",
-    formClass: ".popup__formset",
   },
   popup
 );
@@ -109,7 +136,6 @@ const addValid = new FormValidator(
     inactiveButtonClass: "popup__button_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible",
-    formClass: ".popup__formset",
   },
   addCards
 );
