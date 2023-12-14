7d0cc0cf0828837cf8e466a0f5e82abde8823186
Vinicius Bueno <vi-pr@hotmail.com>
2023-12-14 18:37:33 -0300

    corrections

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,52 +1,57 @@
-const openFormButton = document.querySelector(".profile__edit-button");
-const openAddButton = document.querySelector(".profile__add-button");
-const popup = document.querySelector(".popup_profile");
-const addCards = document.querySelector(".popup_add-cards");
-const closeButton = popup.querySelector(".popup__close-button");
-const closeAdd = addCards.querySelector(".popup__close-add");
-const profileName = document.querySelector(".profile__name");
-const profileAbout = document.querySelector(".profile__about");
-const inputName = document.querySelector(".popup__input_type_name");
-const inputAbout = document.querySelector(".popup__input_type_about");
-const form = document.querySelector(".popup__form_profile");
-const fullImage = document.querySelector(".popup_full");
-document.querySelector(".popup_profile").addEventListener("click", onClick);
-document.querySelector(".popup_full").addEventListener("click", onClick);
-document.querySelector(".popup_add-cards").addEventListener("click", onClick);
+export const openFormButton = document.querySelector(".profile__edit-button");
+export const openAddButton = document.querySelector(".profile__add-button");
+export const popup = document.querySelector(".popup_profile");
+export const addCards = document.querySelector(".popup_add-cards");
+export const closeButton = popup.querySelector(".popup__close-button");
+export const closeAdd = addCards.querySelector(".popup__close-add");
+export const profileName = document.querySelector(".profile__name");
+export const profileAbout = document.querySelector(".profile__about");
+export const inputName = document.querySelector(".popup__input_type_name");
+export const inputAbout = document.querySelector(".popup__input_type_about");
+export const form = document.querySelector(".popup__form_profile");
+export const fullImage = document.querySelector(".popup_full");
+export const cardListSelector = document.querySelector(".elements__container");
+export const popupListSelector = document.querySelector(".popup__container");
+export const cardsForm = document.querySelector("#cards-form");
+export const add = document.querySelector("#add-card");
+export const proInp = document.querySelector(".profile__input");
+// document.querySelector(".popup_profile").addEventListener("click", onClick);
+// document.querySelector(".popup_full").addEventListener("click", onClick);
+// document.querySelector(".popup_add-cards").addEventListener("click", onClick);
 
-openFormButton.addEventListener("click", () => {
-  popup.classList.add("popup__opened");
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-});
+// openFormButton.addEventListener("click", () => {
+//   popup.classList.add("popup__opened");
+//   inputName.value = profileName.textContent;
+//   inputAbout.value = profileAbout.textContent;
+// });
 
-openAddButton.addEventListener("click", () => {
-  addCards.classList.add("popup__opened");
-});
+// openAddButton.addEventListener("click", () => {
+//   addCards.classList.add("popup__opened");
+// });
 
-closeButton.addEventListener("click", () => {
-  popup.classList.remove("popup__opened");
-});
+// closeButton.addEventListener("click", () => {
+//   popup.classList.remove("popup__opened");
+// });
 
-closeAdd.addEventListener("click", () => {
-  addCards.classList.remove("popup__opened");
-});
+// closeAdd.addEventListener("click", () => {
+//   addCards.classList.remove("popup__opened");
+// });
 
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  popup.classList.remove("popup__opened");
-});
+// form.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+//   profileName.textContent = inputName.value;
+//   profileAbout.textContent = inputAbout.value;
+//   popup.classList.remove("popup__opened");
+// });
 
-document.addEventListener("keydown", function (evt) {
-  if (evt.key === "Escape") {
-    popup.classList.remove("popup__opened");
-    fullImage.classList.remove("popup__opened");
-    addCards.classList.remove("popup__opened");
-  }
-});
+// document.addEventListener("keydown", function (evt) {
+//   if (evt.key === "Escape") {
+//     popup.classList.remove("popup__opened");
+//     fullImage.classList.remove("popup__opened");
+//     addCards.classList.remove("popup__opened");
+//   }
+// });
 
-function onClick(evt) {
-  evt.target.classList.remove("popup__opened");
-}
+// function onClick(evt) {
+//   evt.target.classList.remove("popup__opened");
+// }
