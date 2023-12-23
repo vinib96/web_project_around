e933ef380309c2ed185e56c12078054a8bc0f86d
Vinicius Bueno <vi-pr@hotmail.com>
2023-12-23 13:00:10 -0300

    fix webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "../styles/index.css";
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
-import PopupWithImage from "../scripts/PopupWithImage.js";
-import Popup from "../scripts/Popup.js";
-import Section from "../scripts/Section.js";
-import UserInfo from "../scripts/UserInfo.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
+import "./styles/index.css";
+import Card from "../src/scripts/Card.js";
+import FormValidator from "../src/scripts/FormValidator.js";
+import PopupWithImage from "../src/scripts/PopupWithImage.js";
+import Popup from "../src/scripts/Popup.js";
+import Section from "../src/scripts/Section.js";
+import UserInfo from "../src/scripts/UserInfo.js";
+import PopupWithForm from "../src/scripts/PopupWithForm.js";
 import {
   popup,
   addCards,
@@ -16,7 +16,7 @@ import {
   initialCards,
   inputAbout,
   inputName,
-} from "../scripts/constants.js";
+} from "../src/scripts/constants.js";
 
 const popupWithImage = new PopupWithImage(fullImage);
 popupWithImage.setEventListeners();
---
+++ src/scripts/constants.js
@@ -0,0 +1,37 @@
+export const openFormButton = document.querySelector(".profile__edit-button");
+export const openAddButton = document.querySelector(".profile__add-button");
+export const popup = document.querySelector(".popup_profile");
+export const addCards = document.querySelector(".popup_add-cards");
+
+export const inputName = document.querySelector(".popup__input_type_name");
+export const inputAbout = document.querySelector(".popup__input_type_about");
+
+export const fullImage = document.querySelector(".popup_full");
+export const cardListSelector = document.querySelector(".elements__container");
+
+export const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
